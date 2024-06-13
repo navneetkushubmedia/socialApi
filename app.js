6ef10ff7aec1e220710090c4501cea835656fd97
James Caaron <jamescaron91@gmail.com>
2024-06-13 12:18:07 +0530

    App.js Changes

--- app.js
+++ app.js
@@ -3,21 +3,18 @@ const os = require('os');
 const bodyParser = require('body-parser');
 var device = require('express-device');
 const fs = require('fs');
-//var http = require('http');
-var http = require('https');
-var cors = require('cors');   // For cross origin device access
+var https = require('https');
+var cors = require('cors'); // For cross origin device access
 var path = require('path');
 var mongoose = require('mongoose');
 var request = require('request');
-var multer  =   require('multer'); 
+var multer = require('multer');
 
 const myEnv = require('dotenv').config();
 
-var db = require('./src/modules/db');  // DB config module
+var db = require('./src/modules/db'); // DB config module
 
 const Route = require('./src/router/Route');
-
-// const localIPAddress = '192.168.29.39';
 
 function getLocalIPAddress() {
   const interfaces = os.networkInterfaces();
@@ -31,16 +28,21 @@ function getLocalIPAddress() {
   return '127.0.0.1'; // fallback to localhost
 }
 
+var port = process.env.PORT || 80; // Port used for user server
 
-var port = process.env.PORT || 80;   // Port used for user server
-var app = express()
-  , server = require('http').createServer(app);;
+// Load SSL certificate and key
+const options = {
+  key: fs.readFileSync('/path/to/your/private-key.pem'),
+  cert: fs.readFileSync('/path/to/your/certificate.pem'),
+  ca: fs.readFileSync('/path/to/your/ca_bundle.pem')
+};
 
-// server.listen(port);
+var app = express(),
+  server = https.createServer(options, app);
 
-server.listen(port, () => {
+server.listen(port, '0.0.0.0', () => {
   const ip = getLocalIPAddress();
-  console.log(`Server running at http://${ip}:${port}/`);
+  console.log(`Server running at https://${ip}:${port}/`);
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -50,7 +52,6 @@ app.use(express.static(dir));
 
 app.use('/uploads', express.static('uploads'));
 
-
 app.use(cors());
 app.use(device.capture());
 
@@ -59,9 +60,15 @@ app.use(express.json({ rejectUnauthorized: false }));
 
 app.set('port', port);
 
-
-app.get('/', function (req, res) { console.log("User Hit"); res.send('<h1>Hello Account</h1>'); });
+app.get('/', function (req, res) {
+  console.log("User Hit");
+  res.send('<h1>Hello Account</h1>');
+});
 
 app.use('/', Route);
 
-
+// Error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
