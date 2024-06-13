b8f452f9a8bcfd80002cdc245b15cafe7e743b2b
James Caaron <jamescaron91@gmail.com>
2024-06-13 12:09:00 +0530

    Get Ip

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const os = require('os');
 const bodyParser = require('body-parser');
 var device = require('express-device');
 const fs = require('fs');
@@ -17,11 +18,30 @@ var db = require('./src/modules/db');  // DB config module
 const Route = require('./src/router/Route');
 
 // const localIPAddress = '192.168.29.39';
+
+function getLocalIPAddress() {
+  const interfaces = os.networkInterfaces();
+  for (let iface in interfaces) {
+    for (let alias of interfaces[iface]) {
+      if (alias.family === 'IPv4' && !alias.internal) {
+        return alias.address;
+      }
+    }
+  }
+  return '127.0.0.1'; // fallback to localhost
+}
+
+
 var port = process.env.PORT || 80;   // Port used for user server
 var app = express()
   , server = require('http').createServer(app);;
 
-server.listen(port);
+// server.listen(port);
+
+server.listen(port, () => {
+  const ip = getLocalIPAddress();
+  console.log(`Server running at http://${ip}:${port}/`);
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
